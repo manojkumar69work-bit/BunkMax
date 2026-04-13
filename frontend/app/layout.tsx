9df5e87f6671452f7a2f2221140003e3446b9ce9
MANOJ ETHINI <manojkumarethini@MANOJs-MacBook-Air.local>
2026-04-13 21:28:19 +0530

    updated manifest

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -4,11 +4,11 @@ import Providers from "./providers";
 export const metadata = {
   title: "BunkMax",
   description: "Smart attendance planner",
+  manifest: "/manifest.json",
   icons: {
     icon: "/favicon.ico",
     apple: "/apple-touch-icon.png",
   },
-  manifest: "/manifest.json", // optional but recommended
 };
 
 export default function RootLayout({
@@ -20,9 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Providers>
-          <div className="max-w-md mx-auto min-h-screen pb-20">
-            {children}
-          </div>
+          <div className="max-w-md mx-auto min-h-screen pb-20">{children}</div>
         </Providers>
       </body>
     </html>
